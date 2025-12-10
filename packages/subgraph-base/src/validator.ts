594c43fe082c888bb1af73e3a31e3fac30ea561d
OneTrueHomie <vicigwilo@gmail.com>
2025-12-10 01:01:04 +0100

    chore(subgraph-base): tweak validator mapping

--- packages/subgraph-base/src/validator.ts
+++ packages/subgraph-base/src/validator.ts
@@ -1,13 +1,13 @@
-import { MessageApproved as MessageApprovedEvent } from "../generated/BridgeValidator/BridgeValidator"
-import { MessageApproved } from "../generated/schema"
+import { MessageRegistered as MessageRegisteredEvent } from "../generated/BridgeValidator/BridgeValidator"
+import { MessageRegistered } from "../generated/schema"
 
-export function handleMessageApproved(event: MessageApprovedEvent): void {
-  let entity = new MessageApproved(
+export function handleMessageRegistered(event: MessageRegisteredEvent): void {
+  let entity = new MessageRegistered(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
 
   entity.messageHash = event.params.messageHash
-  entity.signatures = event.params.signatures
+  entity.outgoingMessagePubkey = event.params.outgoingMessagePubkey
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp

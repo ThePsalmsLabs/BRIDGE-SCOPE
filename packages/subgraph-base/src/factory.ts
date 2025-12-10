a856c201e148d406dc563314b2b9e75afe54c0fb
OneTrueHomie <vicigwilo@gmail.com>
2025-12-10 01:01:01 +0100

    chore(subgraph-base): tweak factory mapping

--- packages/subgraph-base/src/factory.ts
+++ packages/subgraph-base/src/factory.ts
@@ -1,15 +1,14 @@
-import { TwinCreated as TwinCreatedEvent } from "../generated/CrossChainERC20Factory/CrossChainERC20Factory"
-import { TwinCreated } from "../generated/schema"
+import { CrossChainERC20Created as CrossChainERC20CreatedEvent } from "../generated/CrossChainERC20Factory/CrossChainERC20Factory"
+import { CrossChainERC20Created } from "../generated/schema"
 
-export function handleTwinCreated(event: TwinCreatedEvent): void {
-  let entity = new TwinCreated(
+export function handleCrossChainERC20Created(event: CrossChainERC20CreatedEvent): void {
+  let entity = new CrossChainERC20Created(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
 
-  entity.owner = event.params.owner
-  entity.twin = event.params.twin
-  entity.remoteToken = event.params.remoteToken
   entity.localToken = event.params.localToken
+  entity.remoteToken = event.params.remoteToken
+  entity.deployer = event.params.deployer
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
